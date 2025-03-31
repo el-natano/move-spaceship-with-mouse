3aec62f329e04294fe1aca5e707bdcb15b591a76
el-natano <BartonNa816@northwested.org>
2025-03-31 10:29:49 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,83 @@
+# Pygame game template
+
+import pygame
+import sys
+import config  
+import random
+import shapes
+
+def init_game():
+    pygame.init()
+    screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))  
+    pygame.display.set_caption(config.TITLE)
+    return screen
+
+def handle_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                return False
+    return True
+
+def main():
+
+    screen = init_game()
+    clock = pygame.time.Clock() 
+    
+    shapes_list = []
+
+    running = True
+    while running:
+        running = handle_events()
+        screen.fill(config.WHITE)  
+        
+    
+    # shape_type = random.randrange(3)
+    
+    # if shape_type == 0:
+    #     new_shape = {
+    #         'type': 'circle',
+    #         'color':(random.randrange(255), random.randrange(255), random.randrange(255)),
+    #         'position': (random.randrange(config.WINDOW_WIDTH), random.randrange(config.WINDOW_HEIGHT)),
+    #         'radius':50
+    #     }
+    # elif shape_type == 1:
+    #     new_shape = {
+    #         'type': 'rectangle',
+    #         'color': (random.randrange(255), random.randrange(255), random.randrange(255)),
+    #         'position': (random.randrange(config.WINDOW_WIDTH - 100), random.randrange(config.WINDOW_HEIGHT - 100)),
+    #         'width': 100,
+    #         'height':10
+    #     }
+    # elif shape_type == 2:
+    #     new_shape = {
+    #         'type': 'line',
+    #         'color': (random.randrange(255), random.randrange(255), random.randrange(255)),
+    #         'start_pos': (random.randrange(config.WINDOW_WIDTH), random.randrange(config.WINDOW_HEIGHT)),
+    #         'width': 10
+    #     }
+
+    # shapes_list.append(new_shape)
+
+    # for shape in shapes_list:
+    #     if shape['type'] == 'circle':
+    #         shapes.draw_circle(screen,shape)
+    #     elif shape['type'] == 'rectangle':
+    #         shapes.draw_rect(screen,shape)
+    #     elif shape['type'] == 'line':
+    #         shapes.draw_line(screen,shape)
+
+
+        
+        pygame.display.flip()
+
+        
+        clock.tick(config.FPS)
+
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
